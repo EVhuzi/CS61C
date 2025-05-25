92f42d0ef5ce089dfb778afe31d998be7181ea5e
YiWei <3468544738@qq.com>
2025-05-25 23:19:28 +0800

    Lab 01.

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,24 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    if (head == NULL)
+	    return 0;
+    node* hare = head->next;
+    node* tortoise = hare; 
+    if (hare != NULL)
+	    hare = hare->next;
+    while (hare != NULL)
+    {
+	    if (hare == tortoise)
+		    return 1;
+	    else
+	    {
+		    tortoise = tortoise->next;
+		   hare = hare->next;
+		  if (hare!= NULL)
+			 hare = hare->next; 
+	    }
+
+    }
+        return 0;
 }
